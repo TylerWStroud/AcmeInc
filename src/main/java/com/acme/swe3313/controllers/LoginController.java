c16109e7a064021d1c7978cab680e1639d966ea3
JS <49812749+JS00001@users.noreply.github.com>
2024-04-11 18:48:56 -0400

    Add event listeners to ui, add associates class

--- src/main/java/com/acme/swe3313/controllers/LoginController.java
+++ src/main/java/com/acme/swe3313/controllers/LoginController.java
@@ -1,8 +1,18 @@
 package com.acme.swe3313.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class LoginController {
+    @FXML
+    private void onButtonAction(ActionEvent event) {
+        System.out.println("Button clicked!");
+    }
+
+    @FXML
+    private void onInputAction(ActionEvent event) {
+        System.out.println("Input entered!");
+    }
 
 }
--- src/main/java/com/acme/swe3313/models/Associate.java
+++ src/main/java/com/acme/swe3313/models/Associate.java
@@ -1,4 +1,35 @@
 package com.acme.swe3313.models;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Associate {
+    private boolean isLoggedIn = false;
+    private String name;
+    private String email;
+
+
+    private boolean login(String email, String password) throws IOException, ParseException {
+        Object object = new JSONParser().parse(new FileReader("associates.json"));
+
+        JSONObject jsonObject = (JSONObject) object;
+
+        // Loop through the array of associates and check if the email and password match for any of them
+        for (Object associate : (Iterable<?>) jsonObject) {
+            JSONObject associateObject = (JSONObject) associate;
+
+            if (associateObject.get("email").equals(email) && associateObject.get("password").equals(password)) {
+                this.isLoggedIn = true;
+                this.name = (String) associateObject.get("name");
+                this.email = (String) associateObject.get("email");
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
