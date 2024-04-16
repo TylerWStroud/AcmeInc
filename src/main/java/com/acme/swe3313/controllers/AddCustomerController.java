0b666004ae7ce8171da9f86d7330151541b784a4
JS <49812749+JS00001@users.noreply.github.com>
2024-04-16 13:48:02 -0400

    Remove unnecessary imports

--- src/main/java/com/acme/swe3313/controllers/AddCustomerController.java
+++ src/main/java/com/acme/swe3313/controllers/AddCustomerController.java
@@ -5,7 +5,6 @@ import com.acme.swe3313.models.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
